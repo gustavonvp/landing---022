c3448303e3d8e2937c7da751d4da09a1d7fab0bd
Gustavonvp <gustavonvp@gmail.com>
2022-01-16 02:55:50 -0300

    first commit

---
+++ src/app/weather/forecast/forecast.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ForecastService } from '../forecast.service';
+
+@Component({
+  selector: 'app-forecast',
+  templateUrl: './forecast.component.html',
+  styleUrls: ['./forecast.component.css']
+})
+export class ForecastComponent implements OnInit {
+  forecastData = [];
+
+  constructor(forecastService: ForecastService) {
+    forecastService.getForecast().subscribe(forecastData => {
+      this.forecastData = forecastData;
+    });
+  }
+
+  ngOnInit() {}
+}
